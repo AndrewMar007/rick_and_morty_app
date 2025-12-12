5881dfb10c26de83e6a65afaf7e44220506d18f5
AndrewMar007 <andriymarenich1998@gmail.com>
2025-12-12 15:30:39 +0200

    failures_update

--- lib/core/exceptions/failures.dart
+++ lib/core/exceptions/failures.dart
@@ -3,15 +3,15 @@ import 'package:equatable/equatable.dart';
 abstract class Failure extends Equatable {}
 
 class ServerFailure extends Failure {
-  final String failure;
-  ServerFailure({required this.failure});
+
+
   @override
-  List<Object?> get props => [failure];
+  List<Object?> get props => [];
 }
 
 class InternetFailure extends Failure {
-  final String failure;
-  InternetFailure({required this.failure});
+  
+  
   @override
-  List<Object?> get props => [failure];
+  List<Object?> get props => [];
 }
