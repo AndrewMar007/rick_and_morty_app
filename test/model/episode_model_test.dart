fef56f9a0ed188ceaa29011c0259a2124849e828
AndrewMar007 <andriymarenich1998@gmail.com>
2025-12-13 17:56:42 +0200

    episode_model_test_fix

--- test/model/episode_model_test.dart
+++ test/model/episode_model_test.dart
@@ -4,19 +4,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:rick_and_morty_app/model/episode_model.dart';
 
 import '../fixtures/fixtures_reader.dart';
+import '../values/values_test.dart';
 
 void main() {
-  EpisodeModel tEpsiodeModel = EpisodeModel(
-      id: 28,
-      name: "The Ricklantis Mixup",
-      airDate: "September 10, 2017",
-      episode: "S03E07",
-      characters: [
-        "https://rickandmortyapi.com/api/character/1",
-        "https://rickandmortyapi.com/api/character/2",
-      ],
-      url: "https://rickandmortyapi.com/api/episode/28",
-      created: "2017-11-10T12:56:36.618Z");
+      const tEpsiodeModel = EpisodeModelValues.episode;
 
       group("fromJson", (){
         test("should return a valid model when JSON isRight", (){
